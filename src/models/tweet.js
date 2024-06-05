a94c71362dff754c19b1c076c316ee0017391d90
arpit140 <arpitsunny6@gmail.com>
2024-06-05 12:08:51 +0530

    Added MOdels

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -4,21 +4,21 @@ const tweetSchema = new mongoose.Schema({
     content: {
         type: String,
         required: true,
+        max: [250, "Tweet cannot be more than 250 words..."]
     },
-    userEmail: {
-        type: String
-    },
-    comments: [
+    hashtags: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Comment'
+            ref: "Hashtag"
         }
     ]
+
+
+
+
+
 }, {timestamps: true});
 
-tweetSchema.virtual('contentWithEmail').get(function process() {
-    return `${this.content} \nCreated by: ${this.userEmail}`;
-})
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
 module.exports = Tweet;
